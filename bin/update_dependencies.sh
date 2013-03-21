998fa08c285a500df603873a7b72c19a0fc44527
Brandon Whaley <redkrieg@gmail.com>
2013-03-21 14:36:21 -0400

    update submodules

--- bin/update_dependencies.sh
+++ bin/update_dependencies.sh
@@ -3,11 +3,4 @@
 cd ~
 pip install -U --user git+git://github.com/Lokaltog/powerline
 pip install -U --user paramiko
-
-#cd ~/.vim/bundle/jedi-vim
-#git pull
-#git submodule update --init
-#
-#cd ~/.vim/bundle/supertab
-#git pull
-#git submodule update --init
+git submodule update --init --recursive
